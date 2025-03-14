fe4077b2a03cc758601031d6fa05f80d359bc0f6
Philip Raab <philip@cathedral.co.za>
2025-03-14 12:17:19 +0200

    code doc

--- src/Util/VSCodiumPatcher.php
+++ src/Util/VSCodiumPatcher.php
@@ -1,7 +1,20 @@
 <?php
 
 /**
+ * Develop
  *
+ * Tinkering development environment. Used to play with or try out stuff.
+ *
+ * PHP version 8.4
+ *
+ * @author Philip Michael Raab<philip@cathedral.co.za>
+ * @package Develop\Tinker
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * @version $Id$
+ * $Date$
  */
 
 declare(strict_types=1);
